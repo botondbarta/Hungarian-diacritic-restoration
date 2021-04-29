4bed9f8580a06ea4b6f068b473dd5f3d961f4f69
dorinapetra <dorinapetra@gmail.com>
2021-04-29 12:14:32 +0200

    add vowels to vocab

--- Hungarian-diacritic-restoration/vocabulary.py
+++ Hungarian-diacritic-restoration/vocabulary.py
@@ -1,20 +1,24 @@
+import os
+import pickle
+
+
 class Vocabulary:
     def __init__(self, **kwargs):
         if 'df' in kwargs:
             df = kwargs['df']
             self.chars = self.create_vocab(df)
-
             self.char_indices = dict((c, i + 1) for i, c in enumerate(self.chars))
             self.char_indices['<PAD>'] = 0
-            # with open(os.getcwd() + '/vocab.pkl', 'wb') as file:
-            #    pickle.dump(self.char_indices, file)
+            with open(os.getcwd() + '/vocab.pkl', 'wb') as file:
+                pickle.dump(self.char_indices, file)
             self.indices_char = dict((i + 1, c) for i, c in enumerate(self.chars))
             self.indices_char[0] = '<PAD>'
-            # with open(os.getcwd() + '/vocab_dec.pkl', 'wb') as file:
-            #    pickle.dump(self.indices_char, file)
+            with open(os.getcwd() + '/vocab_dec.pkl', 'wb') as file:
+                pickle.dump(self.indices_char, file)
         elif 'vocab' in kwargs and 'vocab_dec' in kwargs:
             self.char_indices = kwargs['vocab']
             self.indices_char = kwargs['vocab_dec']
+        self.vowels = [self.char_indices[v] for v in 'aeiou']
 
     def create_vocab(self, df):
         vocab = set()
